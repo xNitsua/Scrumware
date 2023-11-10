9f10f857d9a04bade34550b9f2277b77837d9f70
xNitsua <almilhoan@student.ysu.edu>
2023-11-09 21:34:02 -0500

    Progress on Router Invaders
    Game at roughly 60 percent completion

---
+++ Scrumware CSIS 5801/Assets/Scenes/Virus Invasion/Scripts/shootProjectile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootProjectile : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
